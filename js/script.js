e0b2a9d47f688d643edc90cde8f32ce4fa557f6b
Roman Pecherskikh <pwpec95@gmail.com>
2023-07-05 00:01:33 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.gallery-slider', {
+    slidesPerView: 4,
+    simulateTouch: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    spaceBetween: 10,
+    loop: false,
+});
